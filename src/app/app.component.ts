1ec3a6599e6c23be888bfe9cbe6221bf1e3eedf3
MinhUyen0210 <minhuyen0210@gmail.com>
2020-07-01 12:01:35 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,33 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template:`
+  <h2>Hello there</h2>
+  <h3>Your name: {{ user.name }}</h3>
+    <p>Your age: {{ user.age }}</p>
+
+    <div *ngIf="user.age >= 13"; else noPG13>
+Bạn có thể xem nội dung PG-13    </div>
+<ng-template #noPG13>
+    <div *ngIf="user.age < 13">
+  Bạn không thể xem nội dung PG-13
+</div>
+</ng-template>`,
+
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  user ={
+    name: 'Uyen',
+    age: 11
+  }
+
+  handler(event){
+    console.log("clicked", event);
+  }
+
+  
 }
